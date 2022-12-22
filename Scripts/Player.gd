e43f118890626e09b52e233c2685353c0c1f7422
AndreaTerenz <andrea.terenziani.at@gmail.com>
2022-12-22 19:00:04 +0100

    Removed mouse controlled movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,11 @@ extends CharacterBody3D
 
 @export_range(.01, 20., .005) var TOTAL_SPEED = 5. 
 @export_range(.01, 50., .005) var H_SPEED := 5.
+@export_range(.01, 2., .005) var H_DECELERATION := .5
 @export_range(.01, 20., .005) var JUMP_VELOCITY := 4.5
+
+@onready
+var body := %Body
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,40 +15,32 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	Globals.set_player(self)
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		var viewport := get_viewport()
-		var mouse_pos = viewport.get_mouse_position()
-		var screen_center = viewport.get_visible_rect().size / 2.
-		var delta : Vector2 = mouse_pos - screen_center
-		
-		rotation.y = -delta.angle() + TAU / 8.
-
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var on_floor := is_on_floor()
+	var v_vel := velocity.y
+	var h_vel := Vector2(velocity.x, velocity.z)
 
 	# Get the input direction and handle the movement/deceleration.
-	# Every action is flipped......MMMMMMM
 	var input_dir = Input.get_vector("right", "left", "backward", "forward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		velocity.x = direction.x * H_SPEED
-		velocity.z = direction.z * H_SPEED
+		h_vel = Vector2(direction.x, direction.z) * H_SPEED
+		h_vel = h_vel.rotated(TAU/8.).rotated(TAU/2.)
 	else:
-		velocity.x = move_toward(velocity.x, 0, H_SPEED)
-		velocity.z = move_toward(velocity.z, 0, H_SPEED)
+		h_vel = h_vel.lerp(Vector2.ZERO, H_DECELERATION * float(on_floor))
+		
+	# Add the gravity.
+	if not on_floor:
+		v_vel -= gravity * delta
+	# Handle Jump.
+	elif Input.is_action_just_pressed("jump"):
+		v_vel = JUMP_VELOCITY
 	
+	velocity = Vector3(h_vel.x, v_vel, h_vel.y)
+	
+	if direction:
+		var body_look_pos = Vector3(velocity.x, 0., velocity.z).normalized()
+		body.look_at((global_position - body_look_pos), Vector3.UP)
+		
 	move_and_slide()
-	
-func rotate_with_mouse(event: InputEventMouseMotion, node: Node3D, sensitivity := .001):
-	var tmp = -event.relative * sensitivity
-	var event_rel : Vector2 = Vector2(rad_to_deg(tmp.x), rad_to_deg(tmp.y))
-	var y_rot = event_rel.x
-
-	node.rotate_y(y_rot)
