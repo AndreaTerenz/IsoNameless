2a751b35cafcfc3d553e3d54c45afd1799ecd945
AndreaTerenz <andrea.terenziani.at@gmail.com>
2022-12-23 00:49:55 +0100

    ANOTHER movement rework
    We inspired by Hades now

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 @export_range(.01, 20., .005) var TOTAL_SPEED = 5. 
 @export_range(.01, 50., .005) var H_SPEED := 5.
-@export_range(.01, 2., .005) var H_DECELERATION := .5
+@export_range(.01,  2., .005) var H_DECELERATION := .5
 @export_range(.01, 20., .005) var JUMP_VELOCITY := 4.5
 @export_range(.01, 20., .005) var ROT_SENSITIVITY := 4.
 
@@ -16,7 +16,17 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	Globals.set_player(self)
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		var viewport := get_viewport()
+		var mouse_pos = viewport.get_mouse_position()
+		var screen_center = viewport.get_visible_rect().size / 2.
+		var delta : Vector2 = mouse_pos - screen_center
+		
+		body.rotation.y = -delta.angle() + TAU / 8.
+
 func _physics_process(delta):
+	"""
 	var rot_dir := 0.
 	if Input.is_action_pressed("rot_left"):
 		rot_dir = 1.
@@ -24,6 +34,7 @@ func _physics_process(delta):
 		rot_dir = -1.
 	
 	rotate_y((TAU/8.) * rot_dir * ROT_SENSITIVITY * delta)
+	"""
 		
 	var on_floor := is_on_floor()
 	var v_vel := velocity.y
@@ -46,9 +57,13 @@ func _physics_process(delta):
 		v_vel = JUMP_VELOCITY
 	
 	velocity = Vector3(h_vel.x, v_vel, h_vel.y)
-	
+	if direction:
+		velocity = velocity.rotated(Vector3.UP, TAU/2.-TAU/8.)
+		
+	"""
 	if direction:
 		var body_look_pos = Vector3(velocity.x, 0., velocity.z).normalized()
 		#body.look_at((global_position - body_look_pos), Vector3.UP)
-		
+	"""
+	
 	move_and_slide()
